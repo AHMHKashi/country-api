387fb17d3cb91892e10dcc5b4684b070cc449fe5
Abolfazl Kashi <abolfazlkashi81@gmail.com>
2024-05-21 21:59:18 +0330

    feat: implement caching for api usages

--- src/main/java/com/example/countryapi/repository/WeatherRepository.java
+++ src/main/java/com/example/countryapi/repository/WeatherRepository.java
@@ -3,6 +3,7 @@ package com.example.countryapi.repository;
 import com.example.countryapi.models.Weather;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +20,7 @@ public class WeatherRepository {
         this.ninjasTemplate = ninjasTemplate;
     }
 
+    @Cacheable(value = "weatherInfos", key = "#cityName")
     public Optional<Weather> getWeatherByCityName(String cityName) {
         var result = ninjasTemplate.getForObject(WeatherAPI + "?city=" + cityName, Weather.class);
         if (result != null) {
